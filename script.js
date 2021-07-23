cd4b288ce02f273b36e3d746e38d329daee6a134
sourtaste000 <sourtaste000@gmail.com>
2021-07-23 10:32:10 -0700

    moar animations

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// jQuery hell
 window.onload = () => {
   setTimeout(() => {
     $("#title").slideDown(1000, () => {
@@ -7,14 +8,15 @@ window.onload = () => {
         },
         1000
       );
+      $("html").css("backdrop-filter", "brightness(50%)");
 
       $("#navbar").fadeIn(1000, () => {
-        $("#title").css("border-bottom", "none");
-        setTimeout(() => {
+        $("#footer").fadeIn(1000, () => {
+          // $("#title").css("border-bottom", "none");
           $(".placeholder").remove();
           $("#title").prependTo("#navbar");
           $("#title").css("position", "unset");
-        }, 500);
+        });
       });
     });
   }, 500);
