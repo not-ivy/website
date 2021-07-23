c4b7022332d854c26618585932426a10c86bd20e
sourtaste000 <sourtaste000@gmail.com>
2021-07-22 22:28:39 -0700

    example so i dont forget

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = () => {
   setTimeout(() => {
-    $("#title").slideDown( 1000, function() {
+    $("#title").slideDown( 1000, () => {
       $("#title").animate({
         top: 0,
       }, 1000)
 
-      $("#navbar").fadeIn(1000, function() {
+      $("#navbar").fadeIn(1000, () => {
         $("#title").css("border-bottom", "none")
       })
     });
