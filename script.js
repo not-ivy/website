9c2008b8d475879e67fdd3188d9c977ac278d370
sourtaste000 <sourtaste000@gmail.com>
2021-07-23 09:11:39 -0700

    make less hacky, change bg

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 window.onload = () => {
   setTimeout(() => {
-    $("#title").slideDown( 1000, () => {
-      $("#title").animate({
-        top: 0,
-      }, 1000)
+    $("#title").slideDown(1000, () => {
+      $("#title").animate(
+        {
+          top: 0,
+        },
+        1000
+      );
 
       $("#navbar").fadeIn(1000, () => {
-        $("#title").css("border-bottom", "none")
-      })
+        $("#title").css("border-bottom", "none");
+        setTimeout(() => {
+          $(".placeholder").remove();
+          $("#title").prependTo("#navbar");
+          $("#title").css("position", "unset");
+        }, 500);
+      });
     });
-  }, 500)
-}
+  }, 500);
+};
