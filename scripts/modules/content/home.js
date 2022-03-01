3924de60d65b75eb1b1101cd20b3fa14e8a3d255
sourtaste000 <sourtaste000@i-sp.in>
2022-02-28 21:32:34 -0800

    more content

---
+++ scripts/modules/content/home.js
@@ -0,0 +1,35 @@
+const home = document.getElementById('home');
+
+home.style.display = 'block';
+
+document.getElementById('home-button').addEventListener('click', () => {
+  for (let item of document.getElementsByClassName('content')) {
+    item.style.display = 'none';
+  }
+  home.style.display = 'block';
+});
+
+home.innerHTML = /*html*/`
+<div class="title"><h1>Home</h1></div>
+<p>
+  Welcome to my website! I create programs and website on my free time, and I'm always looking for new projects to work on.<br>
+  Use the buttons above to navigate to the different sections of my website.
+</p>
+
+<hr class="low">
+<h3>Contact</h3>
+<p>
+  Discord: sourTaste000#2391 (&lt;8 hours)
+  <br>
+  Email: <a href="mailto:sourtaste000@i-sp.in">sourtaste000@i-sp.in</a> (&lt;10 hours)
+ <br>
+  Keybase: <a href="https://keybase.io/sussyimpostor">sussyimpostor</a> (&lt;24 hours)
+</p>
+<hr class="low">
+<h3>Links</h3>
+<p>
+  GitHub: <a href="https://github.com/sourTaste000/">sourTaste000</a>
+  <br>
+  PGP Key: <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/2377F28C8BDC3BB7CDE8337CD2A1D05E1B5EAA32">D2A1 D05E 1B5E AA32</a>
+</p>
+`;
