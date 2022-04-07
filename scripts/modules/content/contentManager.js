eed81ba9cccdf5e0e28c1fdfa3a26f0c4821bab0
sourtaste000 <sourtaste000@i-sp.in>
2022-04-06 17:17:16 -0700

    fix issue

--- scripts/modules/content/contentManager.js
+++ scripts/modules/content/contentManager.js
@@ -3,12 +3,6 @@ import EventEmitter from 'events';
 const contentElements = [];
 const buttonEvent = new EventEmitter();
 
-const hideAll = () => {
-  [...contentElements].forEach((element) => {
-    element.style.display = 'none';
-  });
-};
-
 [...document.body.children].forEach((element) => {
   if (element.classList.contains('content')) {
     contentElements.push(element);
@@ -17,13 +11,12 @@ const hideAll = () => {
 
 contentElements.forEach((item) => {
   document.getElementById(`${item.id}-button`).addEventListener('click', () => {
+    [...contentElements].forEach((element) => {
+      element.style.display = 'none';
+    });
+    item.style.display = 'block';
     buttonEvent.emit('click', item);
   });
-});
-
-buttonEvent.on('click', (element) => {
-  hideAll();
-  element.style.display = 'block';
 });
 
 export default buttonEvent;
